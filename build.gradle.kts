94782749e292131921d219744b20e621969dbda5
donghoon <1031nice@gmail.com>
2026-04-05 01:03:06 +0900

    chore: upgrade project java version to 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,9 @@ group = "com.mall"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
 }
 
 repositories {
